37f0fa67179ebcbf647b9cf435cc2f7938519229
Aditya Bhatt <aditya.bhatt@kolabsys.com>
2015-12-18 14:26:54 +0100

    Fix JSHint and JSCS issues

--- addon/components/roundcube-notifications-main.js
+++ addon/components/roundcube-notifications-main.js
@@ -1,16 +1,15 @@
 import Ember from 'ember';
 import layout from '../templates/components/roundcube-notifications-main';
-import Notify from 'ember-notify';
 
 export default Ember.Component.extend({
-  layout: layout,
+  layout,
   pubsub: Ember.inject.service(),
   notify: Ember.inject.service('notify'),
   setup: function() {
-    var pubsub = this.get('pubsub'),
+    let pubsub = this.get('pubsub'),
         notify = this.get('notify');
 
-    pubsub.on('shell.notify', function (message) {
+    pubsub.on('shell.notify', function(message) {
       notify.info(message + '', {
         closeAfter: 5000
       });
